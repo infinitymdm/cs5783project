2c3308c9484f92dfd696a969b59f57f2bdcdef59
Marcus Mellor <infinitymdm@gmail.com>
2024-11-24 18:02:07 -0600

    Update LDM

--- scripts/train_mini_ldms.sh
+++ scripts/train_mini_ldms.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 
-for i in {1..50}
+for i in {1..10}
 do
     echo "Training model $i ..."
+    rm ~/.cache/autoencoders/data/*/.ready
     pushd latent-diffusion
     CUDA_VISIBLE_DEVICES=0 python3 main.py --base ../configs/txt2img-mini.yaml -t --gpus 0, --no-test
     popd

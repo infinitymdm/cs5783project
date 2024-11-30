6d7e283dadcbc47a98a4c9935667be1814bbc983
Jacob Pease <jacobpease@protonmail.com>
2024-11-30 13:07:31 -0600

    Changes to latent-diffusion and main.py for greedy soups.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import clip
 import os
 import json
 import operator
+
+import pathlib
 from pathlib import Path
 from omegaconf import OmegaConf
 
@@ -40,6 +42,7 @@ def parse_arguments():
         help="The root directory for the datasets.",
     )
     parser.add_argument(
+        "-m",
         "--model-location",
         type=str,
         default=os.path.expanduser('~/models'),
@@ -50,6 +53,16 @@ def parse_arguments():
     #    "--download-models", action="store_true", default=False,
     #)
 
+    parser.add_argument(
+        "-l",
+        "--logdir",
+        type=str,
+        const=True,
+        default="",
+        nargs="?",
+        help="Log directory for individual models",
+    )
+    
     parser.add_argument(
         "--eval-individual-models", action="store_true", default=False,
     )
@@ -72,6 +85,14 @@ def parse_arguments():
     )
 
     parser.add_argument(
+        "-r",
+        "--results",
+        type=str,
+        default=os.path.relpath('results/'),
+        help="Directory for storing intermediate greedy soup results",
+    )
+
+    parser.add_argument(
         "--batch-size",
         type=int,
         default=256,
@@ -86,9 +107,17 @@ def parse_arguments():
 def get_model_paths(models_dir):
     return [Path(c) for c in models_dir.glob('**/*.ckpt')]
 
+def get_eval_paths(eval_dir):
+    return [Path(c) for c in eval_dir.glob('**/**/*.csv')]
+
 def main(args):
+    INDIVIDUAL_MODEL_RESULTS_FILE = 'individual_model_results.jsonl'
+    UNIFORM_SOUP_RESULTS_FILE = 'uniform_soup_results.jsonl'
+    GREEDY_SOUP_RESULTS_FILE = 'greedy_soup_results.jsonl'
     # Define model path
     models_dir = Path(args.model_location)
+    logdir = Path(args.logdir) if args.logdir else None
+    print(logdir)
     # Load the LDM config.
     config = OmegaConf.load(args.config)
 
@@ -96,10 +125,42 @@ def main(args):
 
     # Instantiate the LDM model. This model is a Pytorch Lightning model
     base_model = instantiate_from_config(config.model)
+    model_paths = get_model_paths(models_dir)
+    NUM_MODELS = len(model_paths)
 
+    # Evlatuate Individual Models -------------------------------------
+    if args.eval_individual_models:
+        if os.path.exists(INDIVIDUAL_MODEL_RESULTS_FILE):
+            os.remove(INDIVIDUAL_MODEL_RESULTS_FILE)
+
+        if not logdir:
+            raise exception("Need to have a log directory to create sorted list.")
+
+        print("HERE")
+        eval_paths = get_eval_paths(logdir)
+
+        #eval_dirs = [x[0] for x in os.walk(logdir)]
+        eval_dirs = next(os.walk(logdir))[1]
+
+        print(eval_dirs)
+
+        losses = []
+        for j, eval_path in enumerate(eval_paths):
+            #print(f"{j}, {eval_path}")
+            df = pd.read_csv(eval_path)
+            print(f"{eval_path}\n {df['val/loss'][0]}")
+            losses.append(df['val/loss'][0])
+
+        losses_list = dict(zip(eval_dirs, losses))
+        print(losses_list)
+
+        with open(INDIVIDUAL_MODEL_RESULTS_FILE, 'a+') as f:
+            f.write(json.dumps(losses_list) + '\n')
+
+    # Uniform soup ----------------------------------------------------
     if args.uniform_soup:
-        model_paths = get_model_paths(models_dir)
-        NUM_MODELS = len(model_paths)
+        #model_paths = get_model_paths(models_dir)
+        #NUM_MODELS = len(model_paths)
         #base_model = torch.load(model_paths[0], map_location="cpu")
         for j, model_path in enumerate(model_paths):
             model_dict = torch.load(model_path, map_location="cpu")["state_dict"]
@@ -115,6 +176,55 @@ def main(args):
         model.load_state_dict(uniform_soup)
 
         torch.save(uniform_soup, "uniform_soup.pt")
+
+        
+    # Greedy soup -----------------------------------------------------
+    if args.greedy_soup:
+        # Reset greedy soup results file
+        if os.path.exists(GREEDY_SOUP_RESULTS_FILE):
+            os.remove(GREEDY_SOUP_RESULTS_FILE)
+
+        # Reset results directory
+        if os.path.exists(args.results):
+            pathlib.Path(args.results).rmdir(recursive=True)
+            os.mkdir(args.results)
+
+        # This series of functions reads the output JSON into a Pandas DataFrame
+        # but it reads it in a different format than what it wrote the dict in.
+        # I first convert the dataframe back into the correct dictionary form,
+        # then conver the dictionary into a list of tuples, sorting by the second
+        # element. I then recover the sorted diretory names.
+
+        # If we had multiple datasets we validated on, this would have to be different.
+        individual_model_db = pd.read_json(INDIVIDUAL_MODEL_RESULTS_FILE, lines=True)
+        individual_model_val_accs = individual_model_db.to_dict(orient="records")[0]
+        sorted_models = list(individual_model_val_accs.items())
+        sorted_models.sort(key = lambda a : a[1])
+        sorted_models = list(zip(*sorted_models))[0]
+
+        individual_model_val_accs = sorted(individual_model_val_accs.items(), key=operator.itemgetter(1))
+        # Start the soup by using the first ingredient.
+        greedy_soup_ingredients = [sorted_models[0]]
+        greedy_soup_params = torch.load(os.path.join(args.model_location, f'{sorted_models[0]}/model.ckpt'))['state_dict']
+        best_val_acc_so_far = individual_model_val_accs[0][1]
+        #held_out_val_set = ImageNet2p(preprocess, args.data_location, args.batch_size, args.workers)
+
+        for i in range(1, NUM_MODELS):
+            print(f'Testing model {i} of {NUM_MODELS}')
+
+            new_ingredient_params = torch.load(os.path.join(args.model_location, f'{sorted_models[i]}/model.ckpt'))['state_dict']
+            num_ingredients = len(greedy_soup_ingredients)
+            potential_greedy_soup_params = {
+                k : greedy_soup_params[k].clone() * (num_ingredients / (num_ingredients + 1.)) +
+                    new_ingredient_params[k].clone() * (1. / (num_ingredients + 1))
+                for k in new_ingredient_params
+            }
+
+            torch.save(potential_greedy_soup_params, os.path.join(args.results_location, "greedy_soup_temp.pt"))
+            os.system(f"CUDA_VISIBLE_DEVICES=0 python3 ./latent-diffusion/main.py -r {args.results + 'greedy_soup_temp.pt'} -l {args.results} -b {args.config}")
+            exit()
+            
+
         
         
 
--- scripts/eval_models.sh
+++ scripts/eval_models.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# Run this script from the main repo directory.
+REPO_DIR=$(pwd)
 CKPT_LOCATION=/opt/ckpts
 OUTPUTFILE=./outfile
+LOGDIR=$REPODIR/logs
+
+if [ -e $LOGDIR ]; then
+    rm -rf $LOGDIR
+fi
+
+mkdir $LOGDIR
 
 declare -a arr=($(ls $CKPT_LOCATION | xargs))
 print arr
@@ -9,7 +18,7 @@ for i in "${arr[@]}"
 do
     model="$CKPT_LOCATION/$i/model.ckpt"
     echo $model
-    loss=$(CUDA_VISIBLE_DEVICES=0 python3 main.py -r "$CKPT_LOCATION/$i/model.ckpt" -b ../configs/txt2img-mini.yaml | grep "/loss\s" | xargs | cut -d " " -f 2)
+    loss=$(CUDA_VISIBLE_DEVICES=0 python3 main.py -n "$i" -r "$CKPT_LOCATION/$i/model.ckpt" -l $LOGDIR -b ../configs/txt2img-mini.yaml)
     echo "model_$i : $loss" >> $OUTPUTFILE
 done
 popd

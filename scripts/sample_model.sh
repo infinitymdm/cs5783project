cb314687fcdda4082080044370450b1a5eedb6bd
Marcus Mellor <infinitymdm@gmail.com>
2024-12-06 15:59:25 -0600

    scripts: Working unconditional sampling

--- scripts/sample_model.sh
+++ scripts/sample_model.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 pushd latent-diffusion
-CUDA_VISIBLE_DEVICES=0 python3 scripts/sample_diffusion.py -r "$1" -n 4 -v --batch_size 1
+CUDA_VISIBLE_DEVICES=0 python3 scripts/sample_diffusion.py -r "$1" -n 4 -c 100 --eta 1.0 --batch_size 1
 popd
--- scripts/test_model.sh
+++ scripts/test_model.sh
@@ -1,3 +1,3 @@
 pushd latent-diffusion
-CUDA_VISIBLE_DEVICES=0 python3 main.py -r ../uniform_soup.pt -b ../configs/txt2img-mini.yaml
+CUDA_VISIBLE_DEVICES=0 python3 main.py -r "$1" -b "$1/config.yaml"
 popd

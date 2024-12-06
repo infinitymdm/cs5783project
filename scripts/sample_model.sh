2fc89e19aa09a22179f18c6137cd29e273bb6a35
Jacob Pease <japease@flandre.ecen.okstate.edu>
2024-12-06 13:31:31 -0600

    Preparing to fine tune models.

--- scripts/sample_model.sh
+++ scripts/sample_model.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 pushd latent-diffusion
-CUDA_VISIBLE_DEVICES=0 python3 scripts/sample_diffusion.py -r "$1" -n 4 --batch_size 1 -c 20 -e 0.0
+CUDA_VISIBLE_DEVICES=0 python3 scripts/sample_diffusion.py -r "$1" -n 4 -v --batch_size 1
 popd
